a838eb0c3ec5b13914ef2a0c424ce8d506861c49
ask098 <cmlocastro20@gmail.com>
2019-03-09 23:45:15 -0500

    add navigationbar

---
+++ src/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="navigation" class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="index.html" class="navbar-brand"><span>wayuu</span> App</a>
+		</div>
+
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#home" class="smoothScroll">Home</a></li>
+				<li><a href="#about" class="smoothScroll">About</a></li>
+			</ul>
+		</div>
+
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Home from './Home.vue'
+import Navigation from './Navigation.vue'
 
 
 new Vue({
@@ -10,4 +11,8 @@ new Vue({
 new Vue({
   el: '#home',
   render: h => h(Home)
+})
+new Vue({
+  el: '#navigation',
+  render: h => h(Navigation)
 })
