b890e8c305112bb1d4f6766541f9d6b5a04b0a34
ask098 <cmlocastro20@gmail.com>
2019-03-10 13:32:18 -0500

    management vue, checking for unfolderd list + option for each entity

---
+++ src/Entities/School.vue
@@ -0,0 +1,13 @@
+<template>
+    <form >
+        <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" required>
+        <input name="email" type="email" class="form-control" id="email" placeholder="Email Address" required>
+        
+    </form>
+</template>
+<script>
+export default {
+    name: 'entity',
+
+}
+</script>
--- src/Management.vue
+++ src/Management.vue
@@ -9,14 +9,10 @@
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h2 class="modal-title">administración de escuelas</h2>
                </div>
-
-                    <form action="#" method="post">
-                         <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" required>
-                         <input name="email" type="email" class="form-control" id="email" placeholder="Email Address" required>
-                         <textarea name="message" rows="3" class="form-control" id="message" placeholder="Message" required></textarea>
-                         <input name="submit" type="submit" class="form-control" id="submit" value="Send Message">
-                    </form>
-               </div>
+                <form action="#" method="post">
+                    <input name="submit" type="submit" class="form-control" id="submit" value="Send Message">
+               </form>
+           </div>
           </div>
      </div>
 </div>
@@ -31,7 +27,15 @@ export default {
           name: 'Your Name',
           email: 'Email Address',
           message: 'Message',
-          submit: 'Send Message'
+          submit: 'Send Message',
+          entities: [
+               { name: 'School', value: 'school', id: 1},
+               { name: 'Teachers', value: 'teacher', id: 2},
+               { name: 'Courses', value: 'courses', id: 3},
+               { name: 'Students', value: 'students', id: 4}
+          ],
+          selectedEntity: 'school',
+          loading: true
           }
      }
 }
